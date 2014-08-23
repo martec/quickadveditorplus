670b42e8ee6c05f21407034fb78fb7d82666dc55
martec <mr.martchero@gmail.com>
2014-08-23 16:52:56 +0900

    initial commit

---
+++ inc/languages/english/admin/config_quickadveditorplus.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$l['quickadveditorplus_smile_title'] = 'Show Smile Box in Quick Reply';
+$l['quickadveditorplus_smile_desc'] = 'Set to no if you do not want to show the smile box in quick reply.';
+$l['quickadveditorplus_qedit_title'] = 'Show SCEditor in Quick Edit';
+$l['quickadveditorplus_qedit_desc'] = 'Set to no if you do not want to show the sceditor in quick edit.';
+
+?>
