850b5bb7d2b1e6efec11e592345dbd999157e86d
martec <mr.martchero@gmail.com>
2014-09-29 14:45:30 +0900

    source mode issue again change version

--- inc/plugins/quickadveditorplus.php
+++ inc/plugins/quickadveditorplus.php
@@ -7,7 +7,7 @@
  * @Quick Advanced Editor Plus
  * @author	martec
  * @license http://www.gnu.org/copyleft/gpl.html GPLv3 license
- * @version 1.6
+ * @version 1.7
  * @Special Thanks: Aries-Belgium http://mods.mybb.com/view/quickquote
  */
 
@@ -17,7 +17,7 @@ if(!defined("IN_MYBB"))
 	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
 }
 
-define('QAEP_PLUGIN_VER', '1.6');
+define('QAEP_PLUGIN_VER', '1.7');
 
 // Plugin info
 function quickadveditorplus_info ()
